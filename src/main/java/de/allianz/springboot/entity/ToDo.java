3a175e4c385afb913399be68d1296f16402912a1
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-21 11:36:34 +0100

    ToDo-Klasse mit entsprechenden Annotationen.

---
+++ src/main/java/de/allianz/springboot/entity/ToDo.java
@@ -0,0 +1,28 @@
+package de.allianz.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String creationDate;
+    private String dueDate;
+    private Integer priority;
+    private Boolean isDone;
+
+}
