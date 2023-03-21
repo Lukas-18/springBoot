d6f3f8f691c11320b71c0a215c0d4737bc4628c1
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-21 16:00:43 +0100

    Added DatabasePopulation.

---
+++ src/main/java/de/allianz/springboot/database/DatabasePopulator.java
@@ -0,0 +1,30 @@
+package de.allianz.springboot.database;
+
+import de.allianz.springboot.entity.ToDo;
+import de.allianz.springboot.repository.ToDoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class DatabasePopulator implements CommandLineRunner {
+
+    // DEPENDENCY INJECTION
+    private final ToDoRepository toDoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        final ToDo todo1 = new ToDo(null, "Wäsche", "Wäsche waschen", "21.03.2023", "24.03.2023", 2, false);
+        final ToDo todo2 = new ToDo(null, "Putzen", "Wohnung putzen", "21.03.2023", "31.03.2023", 1, false);
+        final ToDo todo3 = new ToDo(null, "Kochen", "Abendessen kochen", "21.03.2023", "21.03.2023", 3, false);
+
+        toDoRepository.save(todo1);
+        toDoRepository.saveAll(Arrays.asList(todo2, todo3));
+        System.out.println("Anzahl Todos: " + toDoRepository.count());
+    }
+}
