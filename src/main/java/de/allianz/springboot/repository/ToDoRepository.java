76a20aa6deffe9dc5a2a76af65b46a1e9995ac8b
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-22 09:29:41 +0100

    Added Derived Methods

--- src/main/java/de/allianz/springboot/repository/ToDoRepository.java
+++ src/main/java/de/allianz/springboot/repository/ToDoRepository.java
@@ -4,6 +4,26 @@ import de.allianz.springboot.entity.ToDo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ToDoRepository  extends CrudRepository<ToDo, Long> {
+
+    //derived methods = abgeleitete Methoden, im Hintergrund Java Methode wird übersetzt in SQL Statement zur Laufzeit
+    List<ToDo> findAllByIsDoneIsTrue();
+
+    List<ToDo> findAllByIsDoneIsFalse();
+
+
+    //Im Hintergrund hinter der derived method passiert folgendes:
+    //@Query(value = "SELECT COUNT(*) FROM to_do WHERE to_do.status = true", nativeQuery = true)
+    //Long manualCount();
+    Long countAllByIsDoneIsTrue();
+
+
+
+    //generischer Ansatz:
+    // (Also anstatt: "Long countAllByIsDoneIsTrue();" & "Long countAllByIsDoneIsFalse();")
+    Long countAllByIsDone(Boolean staus);
+
 }
