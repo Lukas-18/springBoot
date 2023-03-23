61e6e8ac36e83097500813b358c32189a5505ecf
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-23 16:25:38 +0100

    Added Response Entity (so you can set the HTTP-Status in the Controller-class).

--- src/main/java/de/allianz/springboot/controller/ToDoController.java
+++ src/main/java/de/allianz/springboot/controller/ToDoController.java
@@ -3,13 +3,12 @@ package de.allianz.springboot.controller;
 import de.allianz.springboot.dto.ToDoCreationRequest;
 import de.allianz.springboot.dto.ToDoUpdateRequest;
 import de.allianz.springboot.entity.ToDo;
-import de.allianz.springboot.repository.ToDoRepository;
 import de.allianz.springboot.service.ToDoService;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,50 +21,51 @@ public class ToDoController {
     private final ModelMapper modelMapper;
 
     @PostMapping
-    public ToDo createToDo(@Valid @RequestBody ToDoCreationRequest toDoCreationRequest){
-        return this.toDoService.createToDo(modelMapper.map(toDoCreationRequest, ToDo.class));
+    public ResponseEntity<ToDo> createToDo(@Valid @RequestBody ToDoCreationRequest toDoCreationRequest){
+        return new ResponseEntity<>(this.toDoService.createToDo(modelMapper.map(toDoCreationRequest, ToDo.class)), HttpStatus.CREATED);
     }
 
     @PutMapping
-    public ToDo updateToDo(@Valid @RequestBody ToDoUpdateRequest toDoUpdateRequest){
+    public ResponseEntity<ToDo> updateToDo(@Valid @RequestBody ToDoUpdateRequest toDoUpdateRequest){
         ToDo toDo = this.toDoService.getId(toDoUpdateRequest.getId());
         modelMapper.map(toDoUpdateRequest, toDo);
-        return this.toDoService.updateToDo(toDo);
+        return new ResponseEntity<>(this.toDoService.updateToDo(toDo), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteToDo(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteToDo(@PathVariable("id") Long id){
         this.toDoService.deleteToDo(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public List<ToDo> getAllToDos(){
-        return (List<ToDo>) this.toDoService.getAllToDos();
+    public ResponseEntity<List<ToDo>> getAllToDos(){
+        return new ResponseEntity<>(this.toDoService.getAllToDos(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ToDo getToDoById(@PathVariable ("id") Long id){
-        return this.toDoService.getId(id);
+    public ResponseEntity<ToDo> getToDoById(@PathVariable ("id") Long id){
+        return new ResponseEntity<>(this.toDoService.getId(id), HttpStatus.CREATED);
     }
 
     @GetMapping("/isDone")
-    public List<ToDo> getDoneToDos(){
-        return this.toDoService.getDoneToDos();
+    public ResponseEntity<List<ToDo>> getDoneToDos(){
+        return new ResponseEntity<>(this.toDoService.getDoneToDos(), HttpStatus.OK);
     }
 
     @GetMapping("/isOpen")
-    public List<ToDo> getOpenToDos(){
-        return this.toDoService.getOpenToDos();
+    public ResponseEntity<List<ToDo>> getOpenToDos(){
+        return new ResponseEntity<>(this.toDoService.getOpenToDos(), HttpStatus.OK);
     }
 
     @GetMapping("/isDone/count")
-    public Long getNumberOfDoneToDos(){
-        return this.toDoService.getNumberOfDoneToDos();
+    public ResponseEntity<Long> getNumberOfDoneToDos(){
+        return new ResponseEntity<>(this.toDoService.getNumberOfDoneToDos(), HttpStatus.OK);
     }
 
     @GetMapping("/isOpen/count")
-    public Long getNumberOfOpenToDos(){
-        return this.toDoService.getNumberOfOpenToDos();
+    public ResponseEntity<Long> getNumberOfOpenToDos(){
+        return new ResponseEntity<>(this.toDoService.getNumberOfOpenToDos(), HttpStatus.OK);
     }
 
 }
--- src/main/java/de/allianz/springboot/exceptionHandling/GlobalExceptionHandler.java
+++ src/main/java/de/allianz/springboot/exceptionHandling/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -29,6 +30,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private  final MessageSource messageSource;
 
     @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> entityNotFoundException(RuntimeException exception, WebRequest request) {
         //System.out.println("ENTITY NOT FOUND!");
         //return null;
