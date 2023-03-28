ec10c82dd08ec4ceaf9b4702d1242d2556902555
Lukas Makowski <lukasmakowski4@gmail.com>
2023-03-28 17:01:36 +0200

    Added index.html (still Spring Boot Security)

--- src/main/java/de/allianz/springboot/config/SecureConfig.java
+++ src/main/java/de/allianz/springboot/config/SecureConfig.java
@@ -2,6 +2,7 @@ package de.allianz.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,11 +13,13 @@ public class SecureConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests()
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/index.html")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
-                .and()
-                .httpBasic();
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
